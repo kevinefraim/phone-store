fb55c52d92224dc59f61c7be86d778c7aa3b86a1
Kevin Efraim <kevinefraim02@gmail.com>
2022-01-24 21:47:04 -0300

    prod images

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,49 @@
+import "./Cards.css";
+
+const Cards = () => {
+  return (
+    <div className="card-container">
+      <div class="shadow" style={{ width: "18rem" }}>
+        <img class="card-img-top" src="" alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+        </div>
+      </div>
+      <div class="shadow" style={{ width: "18rem" }}>
+        <img class="card-img-top" src="..." alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p>PRECIO</p>
+        </div>
+      </div>
+      <div class="shadow" style={{ width: "18rem" }}>
+        <img class="card-img-top" src="..." alt="Card image cap" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
+<div className="card-container">
+  <div class="shadow" style={{ width: "18rem" }}>
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+    </div>
+  </div>
+  <div class="shadow" style={{ width: "18rem" }}>
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+    </div>
+  </div>
+  <div class="shadow" style={{ width: "18rem" }}>
+    <img class="card-img-top" src="..." alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+    </div>
+  </div>
+</div>;
