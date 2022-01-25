bcce7d9f87652f14f701f92023f40cd91498c71f
Kevin Efraim <kevinefraim02@gmail.com>
2022-01-25 19:31:04 -0300

    cards fixed

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -13,7 +13,7 @@ const Cards = ({ nombre, precio, img, id }) => {
             src={`${process.env.PUBLIC_URL}/assets/productos/${img}.png`}
             alt={nombre}
           />
-          <div className="card-body text-center fs-5 fw-bolder">
+          <div className="card-body  text-center fs-5 fw-bolder">
             <h5 className="fs- fw-bolder">{nombre}</h5>
             <p>${precio}</p>
           </div>
