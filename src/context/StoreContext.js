27a7080a04635ca9b7b53d3eba6d9680ad2d221b
Nicolás <nicojoaquin1998@gmail.com>
2022-01-25 16:19:47 -0300

    Funciones Carrito

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { initialOfertas, initialProducts } from "../data/data";
 
 export const StoreContext = createContext();
@@ -6,9 +6,65 @@ export const StoreContext = createContext();
 const StoreProvider = ({ children }) => {
   const [products, setProducts] = useState(initialProducts);
   const [ofertas, setOfertas] = useState(initialOfertas);
+  const [carrito, setCarrito] = useState(
+    JSON.parse(localStorage.getItem("carrito")) ?? []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito]);
+
+  const handleAdd = (product, qtySetted) => {
+    const exist = carrito.find((car) => car.id === product.id);
+    if (exist) {
+      setCarrito(
+        carrito.map((car) =>
+          car.id === product.id ? { ...car, qty: car.qty + qtySetted } : car
+        )
+      );
+    } else {
+      setCarrito([...carrito, { ...product, qty: qtySetted }]);
+    }
+  };
+
+  const handleAddQty = (product) => {
+    handleAdd(product, 1);
+  };
+
+  const handleSub = (product) => {
+    const prodInCar = carrito.find((car) => car.id === product.id);
+
+    if (prodInCar.qty === 1) {
+      handleDelete(product.id);
+    } else {
+      setCarrito(
+        carrito.map((car) =>
+          car.id === product.id ? { ...car, qty: car.qty - 1 } : car
+        )
+      );
+    }
+  };
+
+  const handleDelete = (id) => {
+    setCarrito(carrito.filter((car) => car.id !== id));
+  };
+
+  const handleClear = () => {
+    setCarrito([]);
+  };
 
   return (
-    <StoreContext.Provider value={{ products, ofertas }}>
+    <StoreContext.Provider
+      value={{
+        products,
+        ofertas,
+        handleAdd,
+        handleAddQty,
+        handleSub,
+        handleDelete,
+        handleClear,
+      }}
+    >
       {children}
     </StoreContext.Provider>
   );
