69d6fc6a14f4d5ada03b24a64ad4e54e8c597502
Kevin Efraim <kevinefraim02@gmail.com>
2022-01-26 14:33:35 -0300

    comentarios context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,27 +5,32 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [users, setUsers] = useState(
     JSON.parse(localStorage.getItem("users")) ?? []
-  );
+  ); //estado de usuarios con LS
   const [activeUser, setActiveUser] = useState(
     JSON.parse(localStorage.getItem("activeUser")) ?? null
-  );
+  ); //estado de usuario activo con LS
 
+  //seteando item users con LS
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
   }, [users]);
 
+  //seteando item activeuser con LS
   useEffect(() => {
     localStorage.setItem("activeUser", JSON.stringify(activeUser));
   }, [activeUser]);
 
+  //funcion para agregar usuario registrado al estado users
   const handleAddUsers = (user) => {
     setUsers([...users, user]);
   };
 
+  //funcion que setea activeuser con el parametro que pasamos
   const handleActiveUser = (user) => {
     setActiveUser(user);
   };
 
+  //funcion que cierra sesion y setea activeuser como null
   const handleLogout = () => {
     setActiveUser(null);
   };
